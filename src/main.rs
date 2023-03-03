9308374958eba9a54aabfd1bf8fade09b9a5630d
João Silveira <joaos20014@gmail.com>
2023-03-03 14:00:36 +0000

    Refactored Main Loop

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,45 @@ use lisp::frontend::parser::{Err as ParserError, Parser};
 
 use lisp::repl::Evaluator;
 
+/// Evaluate the given source code.
+///
+/// # Arguments
+/// - `source`: The source code to evaluate.
+///
+/// # Returns
+/// The result of the evaluation.
+fn eval(source: &str) -> Result<String, String> {
+    let lexer = Lexer::new(&source);
+    let mut parser = Parser::new(lexer);
+    match parser.parse() {
+        Err(err) => {
+            let idx = match &err {
+                ParserError::UnexpectedEndOfFile => source.len(),
+                ParserError::LexerError(err) => err.begin,
+                ParserError::UnexpectedToken(located) => located.begin,
+            };
+            let loc = Location::new("", &source, idx);
+
+            Result::Err(format!("Error: {}: {}.", loc, err))
+        }
+        Ok(ast) => {
+            dbg!(&ast);
+            let evaluator = Evaluator::new(ast);
+            let result = evaluator.eval();
+            Result::Ok(format!("{}", result))
+        }
+    }
+}
+
 fn main() {
+    println!("Welcome to the lisp repl! Type 'exit' to exit.");
     loop {
-        // Print the prompt
         print!("> ");
         std::io::stdout().flush().unwrap_or_else(|err| {
             eprintln!("Error: {}", err);
             std::process::exit(1);
         });
 
-        // Getting input from the user
         let mut source = String::new();
         std::io::stdin()
             .read_line(&mut source)
@@ -24,35 +53,17 @@ fn main() {
                 eprintln!("Error: {}", err);
                 std::process::exit(1);
             });
-        
-        // Check source is valid ascii
-        if !source.is_ascii() {
-            eprintln!("Error: Input must be valid ascii.");
-            continue;
-        }
 
-        // Initializing the lexer and parser
-        let lexer = Lexer::new(&source);
-        let mut parser = Parser::new(lexer);
-
-        // Parsing and evaluating the input
-        match parser.parse() {
-            Err(err) => {
-                let idx = match &err {
-                    ParserError::UnexpectedEndOfFile => source.len(),
-                    ParserError::LexerError(err) => err.begin,
-                    ParserError::UnexpectedToken(located) => located.begin,
-                };
-                let loc = Location::new("", &source, idx);
-                eprintln!("Error: {}: {}.", loc, err);
+        match source.trim() {
+            "exit" => break,
+            _ if !source.is_ascii() => {
+                eprintln!("Error: Input must contain only valid ascii characters.");
                 continue;
             }
-            Ok(ast) => {
-                println!("{:?}", ast);
-                let evaluator = Evaluator::new(ast);
-                let result = evaluator.eval();
-                println!("{}", result);
-            }
-        };
+            _ => match eval(&source) {
+                Err(err) => eprintln!("{}", err),
+                Ok(result) => println!("{}", result),
+            },
+        }
     }
 }
